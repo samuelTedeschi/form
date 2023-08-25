c3d8a5a1f29adeffeded5a5b4b4e8e56fc50b2d2
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-08-24 23:00:25 -0300

    .

--- server.js
+++ server.js
@@ -1,22 +1,7 @@
-const http = require('http');
-const fs = require('fs'); // Módulo para trabalhar com arquivos
-
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        // Ler e enviar o arquivo HTML para o cliente
-        fs.readFile('index.html', 'utf8', (err, content) => {
-            if (err) {
-                res.writeHead(500);
-                res.end('Erro interno do servidor');
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        });
-    }
-});
-
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const express = require("express");
+const path = require("path");
+const app = new express();
+app.use(express.static(path.join(__dirname, "/pages")));
+app.listen(3000, () => {  
+    console.log("Server running on 3000");
 });
