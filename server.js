f9631c47caf24f3356a1d881b3e6a43cec0f541c
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-09-02 18:16:21 -0300

    .

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = new express();
-app.use(express.static(path.join(__dirname, "/pages")));
-app.listen(3000, () => {  
-    console.log("Server running on 3000");
-});
