43186160da0837ca7ea0eedded7643c82a52cc7d
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-09-04 22:42:25 -0300

    .

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const path = require('path')
 
 const server = express();
 
-server.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(3000, () => {
     console.log('Servidor Express rodando na porta 3000');
